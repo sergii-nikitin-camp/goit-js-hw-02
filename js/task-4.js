decfed4066f7300e4671573fe52116306a87dd33
Sergii Nikitin <eettest6@gmail.com>
2021-07-30 21:53:01 +0300

    Initial commit for homework-02

---
+++ js/task-4.js
@@ -0,0 +1,48 @@
+/**
+ * Задание 4
+ *
+ * Напиши функцию formatString(string) которая принимает строку и форматирует ее если необходимо.
+ *
+ * Если длина строки не превышает 40 символов, функция возвращает ее в исходном виде.
+ * Если длина больше 40 символов, то функция обрезает строку до 40-ка символов и добавляет в конец строки троеточие '...', после
+ * чего возвращает укороченную версию.
+ * const formatString = function(string) {
+ *  // твой код
+ * };
+ *
+ * // Вызовы функции для проверки работоспособности твоей реализации.
+ *
+ * console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+ * // вернется оригинальная строка
+ *
+ * console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+ * // вернется форматированная строка
+ *
+ * console.log(formatString('Curabitur ligula sapien.'));
+ * // вернется оригинальная строка
+ *
+ * console.log(
+ *  formatString(
+ *   'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+ *  ),
+ * );
+ * // вернется форматированная строка
+ */
+
+const formatString = function (string) {
+  const LENGTH_LIMIT = 40;
+  console.debug(`String length: ${string.length}`);
+  if (string.length > LENGTH_LIMIT) {
+    return string.substring(0, LENGTH_LIMIT).concat("...");
+  }
+  return string;
+};
+
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+console.log(formatString("Curabitur ligula sapien."));
+console.log(
+  formatString(
+    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+  )
+);
